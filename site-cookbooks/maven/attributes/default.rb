fb8cd81b006558327e59a9fa208d4e8b2246ebad
Stephen Bannasch <stephen.bannasch@gmail.com>
2012-11-28 00:00:17 -0500

    update location of maven3 tarball

--- site-cookbooks/maven/attributes/default.rb
+++ site-cookbooks/maven/attributes/default.rb
@@ -7,6 +7,6 @@ default['maven']['version'] = 2
 default['maven']['m2_home'] = '/usr/local/maven/default'
 default['maven']['2']['url'] = "http://www.apache.org/dist/maven/binaries/apache-maven-2.2.1-bin.tar.gz"
 default['maven']['2']['checksum'] = "b9a36559486a862abfc7fb2064fd1429f20333caae95ac51215d06d72c02d376"
-default['maven']['3']['url'] = 'http://www.apache.org/dist/maven/binaries/apache-maven-3.0.4-bin.tar.gz'
+default['maven']['3']['url'] = 'http://www.apache.org/dist/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz'
 default['maven']['3']['checksum'] =  "d35a876034c08cb7e20ea2fbcf168bcad4dff5801abad82d48055517513faa2f"
 
