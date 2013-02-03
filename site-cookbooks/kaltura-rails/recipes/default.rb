f465bb24444b2f2c373bdf404e7a94ba959f4e1e
Scott Cytacki <scytacki@concord.org>
2013-02-03 00:14:21 -0500

    add recipe to setup kaltura.yml for VPB

---
+++ site-cookbooks/kaltura-rails/recipes/default.rb
@@ -0,0 +1,13 @@
+# create kaltura.yml file in shared folder
+template "/web/portal/shared/kaltura.yml" do
+  source "kaltura.yml.erb"
+  owner "deploy"
+  group "deploy"
+  mode "0600"
+
+  # need to load in the kaltura credentials from the databags
+    variables(
+    :credentials => data_bag_item('credentials', 'kaltura')
+  )
+end
+
