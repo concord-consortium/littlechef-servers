e0f0108ed8af2eb2879041536548de54f1c2e76c
Noah Paessel <knowuh@gmail.com>
2013-02-21 16:42:10 -0500

    Add delayed_job monit resources.

--- site-cookbooks/cc-monit/recipes/default.rb
+++ site-cookbooks/cc-monit/recipes/default.rb
@@ -13,7 +13,14 @@ end
 
 # aws additional monit files:
 node[:cc_monit][:jobs].each do |conf|
+  approot    = "/web/portal"  # TODO: nodification in cc_rails_portal
+  rails_user = "deploy"       # TODO: nodification in cc_rails_portal
+
   monit_monitrc conf do
-    variables({ :category => "system" })
+    variables({
+      :category    => "system",
+      :approot     => approot,
+      :rails_user  => rails_user
+      })
   end
 end
