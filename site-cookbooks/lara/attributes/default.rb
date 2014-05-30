03af785faf90a397d5128c48826c0147b5c9700a
Noah Paessel <knowuh@gmail.com>
2014-05-30 17:00:34 -0400

    Update databags with Concord Portal SSO info. Also AWS-SES keys.

--- site-cookbooks/lara/attributes/default.rb
+++ site-cookbooks/lara/attributes/default.rb
@@ -1,2 +1,3 @@
-default[:lara][:site_name] = "Lightweight Activity Runtime and Authoring"
-default[:lara][:email] = "authoring-help@concord.org"
+default[:lara][:site_name]     = "Lightweight Activity Runtime and Authoring"
+default[:lara][:email]         = "authoring-help@concord.org"
+default[:lara][:sso_client_id] = "authoring-staging"
