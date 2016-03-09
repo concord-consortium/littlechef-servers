71383f2ef111faed09947836fa5c63b699222f18
Scott Cytacki <scytacki@concord.org>
2016-03-09 13:09:11 -0500

    make PassengerMaxRequests configurable

--- site-cookbooks/cc-rails/attributes/default.rb
+++ site-cookbooks/cc-rails/attributes/default.rb
@@ -5,6 +5,7 @@ default[:cc_rails][:db_pool] = 5
 default[:cc_rails][:rds_domain] = "cc-us-east-1"
 default[:cc_rails][:base_uri] = "/"
 default[:rails][:min_instances] = 1
+default[:rails][:max_requests] = 3000
 default[:cc_rails][:static_assets] = true
 default[:cc_rails][:only_use_ssl] = false
 default[:cc_rails][:use_ssl] = false
